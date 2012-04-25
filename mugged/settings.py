d887091b321760493c94ba7adb6186ef097dc025
bradmann <bradmann@gmail.com>
2012-04-25 17:20:47 -0400

    added friends_birthday to scope

--- mugged/settings.py
+++ mugged/settings.py
@@ -161,4 +161,4 @@ LOGGING = {
 FBAPI_APP_ID = '366622033383939'
 FBAPI_APP_SECRET = '27a567f8ff79b22954e64d52ccc8dade'
 FBAPI_APP_NAMESPACE = 'muggedapp'
-FBAPI_SCOPE = ['user_likes', 'user_photos', 'user_photo_video_tags']
+FBAPI_SCOPE = ['user_likes', 'user_photos', 'user_photo_video_tags', 'friends_birthday']
