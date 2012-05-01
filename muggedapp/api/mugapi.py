327bf7a3dca34f1a4713f8001ccc8058c66de879
bradmann <bradmann@gmail.com>
2012-05-01 01:26:58 -0400

    refactored db layer

--- muggedapp/api/mugapi.py
+++ muggedapp/api/mugapi.py
@@ -6,18 +6,12 @@ import requests
 import re
 
 base_uri = 'http://florida.arrests.org/search.php'
-
-def get_age(birthday):
-	month, day, year = birthday.split('/')
-	birthdate = datetime.datetime(int(year), int(month), int(day))
-	delta = datetime.datetime.now() - birthdate
-	return int(delta.days / 365.25)
 	
 def get_birthdate(birthday):
 	if not birthday or len(birthday.split('/')) != 3:
 		return None
 	month, day, year = birthday.split('/')
-	birthdate = datetime.date(int(year), int(month), int(day))
+	return datetime.date(int(year), int(month), int(day))
 	
 def verify(id, mugshot):
 	mugshot = MugshotSearchResult.objects.filter(search__fbuser__fbid=id).get(arrestpath=mugshot)
@@ -39,56 +33,37 @@ def verify_all(id):
 
 def search_and_update(fbuser):
 	id = fbuser['id']
-	first_name = fbuser['first_name']
-	last_name = fbuser['last_name']
-	birthday = fbuser.get('birthday', None)
+	fname = fbuser.get('first_name')
+	mname = fbuser.get('middle_name')
+	lname = fbuser.get('last_name')
+	birthday = fbuser.get('birthday')
 	birthdate = get_birthdate(birthday)
-	gender_map = {'male': 'M', 'female': 'F'}
-	gender = gender_map.get(fbuser.get('gender', None), None)
-	try:
-		dbUser = FacebookUser.objects.get(fbid=id)
-	except:
-		dbUser = FacebookUser(fbid=id)
-		dbUser.save()
-	try:
-		dbSearch = MugshotSearch.objects.get(fbuser=dbUser.fbid)
-	except:
-		dbSearch = MugshotSearch(fbuser=dbUser, fname=first_name, lname=last_name, birthdate=birthdate, gender=gender, last_searched=timezone.now())
-		dbSearch.save()
-		results = search_mugshot_web(first_name, last_name, gender, birthday)
-		if not results: return None
-		for result in results:
-			dbResult = MugshotSearchResult(thumbpath=result['thumbpath'], arrestpath=result['arrestpath'], search=dbSearch)
-			dbResult.save()
-		return results
-	if dbSearch.last_searched > (timezone.now() - datetime.timedelta(1)):
-		dbResults = MugshotSearchResult.objects.filter(search=dbSearch).exclude(matches_user=False)
-		return [res for res in dbResults.iterator()]
-	else:
-		last_searched = dbSearch.last_searched
-		dbSearch.last_searched = timezone.now()
-		dbSearch.save()
-		results = search_mugshot_web(first_name, last_name, gender, birthday, last_searched)
-		if not results: return None
-		for result in results:
-			dbResult = MugshotSearchResult(thumbpath=result['thumbpath'], arrestpath=result['arrestpath'], search=dbSearch)
-			dbResult.save()
-		return results
+	gender = {'male': 'M', 'female': 'F'}.get(fbuser.get('gender'))
+	
+	dbUser, created = FacebookUser.objects.get_or_create(fbid=id)
 
-def search_mugshot_web(first_name, last_name, gender, birthday=None, arrested_after=None):
-	request_params = {'fname': first_name, 'lname': last_name, 'fpartial': 'True'}
-	if birthday: request_params.update({'sex': gender})
-	if birthday and len(birthday.split('/')) == 3:
-		age = get_age(birthday)
-		request_params.update({'minage': age, 'maxage': age})
-	if arrested_after:
-		request_params.update({'startdate': arrested_after.strf_time('%m%%2F%d%%2F%Y'), 'enddate': datetime.datetime.now().strftime('%m%%2F%d%%2F%Y')})
+	dbSearch = MugshotSearch.objects.filter(fbuser=dbUser, fname=fname, mname=mname, lname=lname, birthdate=birthdate, gender=gender)
+	if dbSearch.count() == 0:
+		MugshotSearch.objects.create(fbuser=dbUser, fname=fname, mname=mname, lname=lname, birthdate=birthdate, gender=gender)
+	dbSearch = MugshotSearch.objects.filter(fbuser=dbUser)
+	for search in dbSearch:
+		if search.last_searched < (timezone.now() - datetime.timedelta(1)):
+			search.last_searched=timezone.now()
+			search.save()
+			for request_params in search.search_requests():
+				results = search_mugshot_web(request_params)
+				for result in results:
+					MugshotSearchResult.objects.create(search=search, thumbpath=result['thumbpath'], arrestpath=result['arrestpath'])
+	
+	return MugshotSearchResult.objects.filter(search__fbuser=dbUser).values()	
+
+def search_mugshot_web(request_params):
 	req = requests.get(base_uri, params=request_params)
 	page = req.content
 	imgpaths = re.findall("<img src='(/thumbs/[^']*)'", page)
 	arrestpaths = re.findall("<a href='(/Arrests/[^']*)'", page)
 	if len(imgpaths) == 0:
-		return None
+		return []
 	paths = [{'thumbpath': imgpaths[i], 'arrestpath': arrestpaths[i], 'matches_user': None} for i in xrange(len(imgpaths))]
 	pagenum = 1
 	patharr = paths[:]
--- muggedapp/models.py
+++ muggedapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 class FacebookUser(models.Model):
 	fbid = models.CharField(primary_key=True, max_length=255)
@@ -9,13 +10,30 @@ class FacebookUser(models.Model):
 class MugshotSearch(models.Model):
 	fbuser = models.ForeignKey(FacebookUser)
 	fname = models.CharField(max_length=255)
+	mname = models.CharField(max_length=255, blank=True, null=True)
 	lname = models.CharField(max_length=255)
 	birthdate = models.DateField(blank=True, null=True)
 	gender = models.CharField(max_length=1, blank=True, null=True)
-	last_searched = models.DateTimeField()
+	last_searched = models.DateTimeField(default=datetime.datetime(1970,1,1))
 	
 	class Meta:
 		unique_together = ('fname', 'lname', 'birthdate', 'gender')
+		
+	def search_requests(self):
+		requests = [{'fname': self.fname, 'lname': self.lname}]
+		if self.mname: requests.append({'fname': self.fname, 'lname': self.mname})
+		if '-' in self.lname:
+			requests.append({'fname': self.fname, 'lname': self.lname.split('-')[0]})
+			requests.append({'fname': self.fname, 'lname': self.lname.split('-')[1]})
+		for req in requests:
+			req['fpartial'] = True
+			req['startdate'] = self.last_searched.strftime('%m%%2F%d%%2F%Y')
+			if self.gender: req['sex'] = self.gender
+			if self.birthdate:
+				age = int((datetime.date.today() - self.birthdate).days / 365.25)
+				req['minage'] = age
+				req['maxage'] = age
+		return requests
 		
 class MugshotSearchResult(models.Model):
 	search = models.ForeignKey(MugshotSearch)
