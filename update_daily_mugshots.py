f3b780bacdf7d6c0e04d398d28b37ecbd18ed110
bradmann <bradmann@gmail.com>
2012-05-09 16:53:11 -0400

    removed logging from update script

--- update_daily_mugshots.py
+++ update_daily_mugshots.py
@@ -1,12 +1,6 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mugged.settings")
 from muggedapp.api import mugapi
-import logging
 
 if __name__ == '__main__':
-	logging.basicConfig(filename='/var/log/muggedapp/daily_mugshot_scrapes.log', level=logging.INFO, format='%(asctime)s %(message)s')
-	logging.info('Starting daily mugshot scrape.')
-	uris = mugapi.update_daily_mugshots()
-	logging.info('Daily mugshot scrape completed. The URIs collected are:')
-	for uri in uris:
-		logging.info(uri)
+	mugapi.update_daily_mugshots()
